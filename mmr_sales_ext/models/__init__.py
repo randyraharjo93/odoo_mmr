6dbdfdb68c4985b3c26f2e8cfd71722378988e46
randyraharjo93@gmail.com <randyraharjo93@gmail.com>
2017-06-01 13:50:08 +0700

    [IMP] Add price_security, add code P in sales and in report

--- mmr_sales_ext/models/__init__.py
+++ mmr_sales_ext/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 import sale_order
+import sale_report
 
 # import signature_request
---
+++ mmr_sales_ext/models/sale_report.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import tools
+from odoo import api, fields, models
+
+
+class SaleReport(models.Model):
+    _inherit = "sale.report"
+
+    mmr_code_p = fields.Boolean(string='Code P', readonly=True)
+
+    def _select(self):
+        return super(SaleReport, self)._select() + ", l.mmr_code_p as mmr_code_p"
+
+    def _group_by(self):
+        return super(SaleReport, self)._group_by() + ", l.mmr_code_p"
