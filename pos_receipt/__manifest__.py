9fa5e33ee929966f7bb705103892f883647ebc00
randyraharjo93 <randyraharjo93@gmail.com>
2017-07-05 17:14:33 +0700

    [IMP] POS receipt

---
+++ pos_receipt/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Indonesian - POS',
+    'version': '1.0',
+    'category': 'Localization',
+    'description': """
+POS Receipt
+    """,
+    'author': 'Falinwa',
+    'website': 'http://www.falinwa.com',
+    'depends': [ 'point_of_sale',],
+    'data': [],
+    'qweb': [
+        'static/src/xml/pos_receipt.xml',
+        ],
+    'demo' : [],
+}
