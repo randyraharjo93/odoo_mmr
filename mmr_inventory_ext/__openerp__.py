b5bc1c755a5c3550ebda3e82bf01d637e69d39ad
randyraharjo93 <randyraharjo93@gmail.com>
2017-10-03 00:17:13 +0700

    [ADD] New module

---
+++ mmr_inventory_ext/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# We have dependencies to odoo voip for now because alexandre want to hide this, but later we can remove it
+{
+    "name": "MMR Inventory ext",
+    "version": "1.0",
+    'author': 'Randy',
+    "description": """
+    Module to add additional functionality on Inventory based on MMR
+    """,
+    "depends": [
+        'base',
+        'stock',
+        ],
+    'init_xml': [],
+    'update_xml': [
+        'views/stock_move_views.xml',
+    ],
+    'css': [],
+    'js': [],
+    'installable': True,
+    'qweb': [
+        ],
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
