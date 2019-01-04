c6e5f938a2e006fd83246aba71d7fbe4bffea05b
Randy Raharjo <randy.raharjo@falinwa.com>
2019-01-04 18:31:39 +0700

    [IMP] On progress

--- mmr_inventory_ext/wizard/stock_report.py
+++ mmr_inventory_ext/wizard/stock_report.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
 from odoo.addons import decimal_precision as dp
+from odoo.tools.float_utils import float_round
 
 
 class stock_report(models.TransientModel):
@@ -8,24 +9,40 @@ class stock_report(models.TransientModel):
     _description = "Stock Report"
 
     date_start = fields.Date("Date Start")
-    date_end = fields.Date("Date Start")
+    date_end = fields.Date("Date End")
     product_id = fields.Many2one("product.product", "Product", domain="[('type','=','product')]")
     stock_report_line_ids = fields.One2many("stock.report.line", "stock_report_id", "Movement(s)")
 
     @api.onchange('date_start', 'date_end', 'product_id')
     def check(self):
-        search_terms = []
         if self.product_id:
-            search_terms.append(('product_id', '=', self.product_id.id))
-        if self.date_start:
-            search_terms.append(('in_date', '>=', self.date_start))
-        if self.date_end:
-            search_terms.append(('in_date', '<=', self.date_end))
-        if search_terms:
-            movement_ids = self.env['stock.quant'].search(search_terms)
-            print (movement_ids.ids)
-            for movement_id in movement_ids:
-                print(movement_id)
+            # print(self._compute_quantities_dict(self.product_id, self.date_start, self.date_end))
+            print(self._compute_quantities_dict(self.product_id, self.date_start, self.date_end))
+
+    @api.multi
+    def _compute_quantities_dict(self, product_id, from_date=False, to_date=False):
+        domain_quant_loc, domain_move_in_loc, domain_move_out_loc = product_id._get_domain_locations()
+
+        domain_move_in = [('state', '=', 'done'), ('product_id', 'in', [product_id.id])] + domain_move_in_loc
+        domain_move_out = [('state', '=', 'done'), ('product_id', 'in', [product_id.id])] + domain_move_out_loc
+
+        if from_date:
+            domain_move_in += [('date', '>=', from_date)]
+            domain_move_out += [('date', '>=', from_date)]
+        if to_date:
+            domain_move_in += [('date', '<=', to_date)]
+            domain_move_out += [('date', '<=', to_date)]
+
+        Move = self.env['stock.move']
+
+        print("LEARN------------------------")
+        print(domain_move_in)
+        print(Move.search(domain_move_in))
+        # print(domain_move_out)
+        # print(Move.search(domain_move_out))
+        print([move.quant_ids.ids for move in Move.search(domain_move_in)])
+
+        return True
 
 
 class stock_report_line(models.TransientModel):
