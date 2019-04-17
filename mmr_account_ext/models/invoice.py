ba77fc4260dccb8c36eb075733d79ea22ac0e5a6
randyraharjo93@gmail.com <randyraharjo93@gmail.com>
2019-04-18 02:27:17 +0700

    [IMP] Test Gutter

--- mmr_account_ext/models/invoice.py
+++ mmr_account_ext/models/invoice.py
@@ -13,7 +13,7 @@ class AccountInvoice(models.Model):
     def action_invoice_open(self):
         result = super(AccountInvoice, self).action_invoice_open()
         # MMR Special Split based on sequence suffix
-        # How to
+        #How to do this
         # Prefix "F"
         # Suffix "|%(month)s/%(year)s   "
         if len(self.number.split('|')) > 1:
