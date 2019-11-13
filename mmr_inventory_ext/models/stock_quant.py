3e48a638cf06caca3bc5d96d285a187bcc3f5076
randyraharjo93@gmail.com <randyraharjo93@gmail.com>
2019-11-14 01:04:54 +0700

    [IMP] I forgot, but for now...

---
+++ mmr_inventory_ext/models/stock_quant.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from odoo import api, fields, models
+
+class Quant(models.Model):
+    _inherit = "stock.quant"
+
+    def recompute_inventory_value(self):
+        for quant in self:
+            if quant.company_id != self.env.user.company_id:
+                # if the company of the quant is different than the current user company, force the company in the context
+                # then re-do a browse to read the property fields for the good company.
+                quant = quant.with_context(force_company=quant.company_id.id)
+            quant.inventory_value = quant.product_id.standard_price * quant.qty
