fb66fbfd615d79db072c77f7a74a4c9ca052f313
randyraharjo93@gmail.com <randyraharjo93@gmail.com>
2020-03-30 11:49:10 +0700

    [IMP] As needed

--- partner_credit_limit/models/sale.py
+++ partner_credit_limit/models/sale.py
@@ -3,47 +3,56 @@
 # See LICENSE file for full copyright and licensing details.
 
 from odoo import api, models, _
-from datetime import datetime
-from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DF
 from odoo.exceptions import UserError
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.one
+    @api.multi
     def check_limit(self):
+        """Check if credit limit for partner was exceeded."""
+        self.ensure_one()
+        partners = []
         partner = self.partner_id
+        partner_parent_id = self.partner_id.parent_id
+        partners.append(partner.id)
+        if partner_parent_id:
+            partners.append(partner_parent_id.id)
+            partner = partner_parent_id
+
         moveline_obj = self.env['account.move.line']
         movelines = moveline_obj.\
-            search([('partner_id', '=', partner.id),
-                    ('account_id.user_type_id.name', 'in',
-                    ['Receivable', 'Payable']),
-                    ('full_reconcile_id', '=', False)])
-
+            sudo().search([('partner_id', 'in', partners),
+                    ('account_id.user_type_id.type', 'in', ['receivable', 'payable'])])
+        confirm_sale_order = self.search([('partner_id', 'in', partners),
+                                          ('state', '=', 'sale')])
         debit, credit = 0.0, 0.0
-        today_dt = datetime.strftime(datetime.now().date(), DF)
+        amount_total = 0.0
+        for status in confirm_sale_order:
+            amount_total += status.amount_total
         for line in movelines:
-            credit += line.debit
-            debit += line.credit
+            credit += line.credit
+            debit += line.debit
+        partner_credit_limit = (partner.credit_limit - debit) + credit
+        available_credit_limit = ((partner_credit_limit -
+                                   (amount_total - debit)) + self.amount_total)
 
-        if (credit - debit + self.amount_total) > partner.credit_limit:
+        if (amount_total - debit) > partner_credit_limit:
+            # Consider partners who are under a company.
             if not partner.over_credit:
-                msg = 'Can not confirm Sale Order,Total mature due Amount ' \
-                      '%s as on %s !\nCheck Partner Accounts or Credit ' \
-                      'Limits !' % (credit - debit, today_dt)
-                raise UserError(_('Credit Over Limits !\n' + msg))
-            else:
-                partner.write({
-                    'credit_limit': credit - debit + self.amount_total})
-                return True
-        else:
-            return True
+                msg = 'Your available credit limit'\
+                      ' Amount = %s \nCheck "%s" Accounts or Credit ' \
+                      'Limits.' % (available_credit_limit,
+                                   self.partner_id.name)
+                raise UserError(_('You can not confirm Sale Order. \n' + msg))
+            partner.write({'credit_limit': debit + self.amount_total})
+        return True
 
     @api.multi
     def action_confirm(self):
+        """Extend to check credit limit before confirming sale order."""
         res = super(SaleOrder, self).action_confirm()
         for order in self:
-            if not self.user_has_groups('base.group_erp_manager'):
-                order.sudo().check_limit()
+            order.check_limit()
         return res
