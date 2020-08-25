b09bacd6e5f6ee33cb21afbbf40d5b527cc598e9
Randy Raharjo <randyraharjo@gmail.com>
2020-08-25 23:52:54 +0700

    [IMP] Double Check on Invoice Side

--- partner_credit_limit/models/sale.py
+++ partner_credit_limit/models/sale.py
@@ -24,7 +24,7 @@ class SaleOrder(models.Model):
         moveline_obj = self.env['account.move.line']
         movelines = moveline_obj.\
             sudo().search([('partner_id', 'in', partners),
-                    ('account_id.user_type_id.type', 'in', ['receivable', 'payable']), ('move_id.state', '=', 'posted'), ('invoice_id', '!=', False)])
+                    ('account_id.user_type_id.type', 'in', ['receivable', 'payable']), ('move_id.state', '=', 'posted')])
         confirm_sale_order = self.search([('partner_id', 'in', partners),
                                           ('state', '=', 'sale')])
         debit, credit = 0.0, 0.0
@@ -32,14 +32,16 @@ class SaleOrder(models.Model):
         for status in confirm_sale_order:
             amount_total += status.amount_total
         for line in movelines:
-            if any(ail.sale_line_ids for ail in line.invoice_id.invoice_line_ids):
-                credit += line.credit
-                debit += line.debit
+            credit += line.credit
+            debit += line.debit
         partner_credit_limit = (partner.credit_limit - debit) + credit
         available_credit_limit = ((partner_credit_limit -
                                    (amount_total - debit)) + self.amount_total)
 
-        if (amount_total - debit) > partner_credit_limit:
+        # Checking on 2 sides
+        # A: From the Sales Order PoV, with risk of Invoice without sales order
+        # B: From Invoice point of View, with risk of confirming Sales Order if there is a lot of Confirmed SO
+        if ((amount_total + self.amount_total - debit) > partner_credit_limit) or (partner_credit_limit - self.amount_total < 0):
             # Consider partners who are under a company.
             if not partner.over_credit:
                 msg = 'Your available credit limit'\
