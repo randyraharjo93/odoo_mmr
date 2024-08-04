3599099a21accb9218a82b3eac618f9ba3ba24cc
Randy <randyraharjo93@gmail.com>
2024-08-04 13:00:52 +0700

    [Revert]

--- mmr_sale_history/models/sale_order.py
+++ mmr_sale_history/models/sale_order.py
@@ -10,12 +10,16 @@ class SaleOrderLine(models.Model):
     date_order = fields.Datetime("Order Data", related="order_id.date_order")
     warning = fields.Text("Price Warning")
 
-    @api.onchange("product_id", "price_unit", "order_partner_id")
+    @api.onchange("product_id", "order_partner_id")
     def _onchange_sale_order_line(self):
-        self.warning = '/'
         order_line_ids = self.search([('product_id', '=', self.product_id.id), ('order_partner_id', '=', self.order_partner_id.id), ('state', '=', 'sale')], order="create_date desc")
         self.sale_order_line_history_ids = order_line_ids
         if order_line_ids:
             self.price_unit = order_line_ids[0].price_unit
-            if tools.float_compare(order_line_ids[0].price_unit, self.price_unit, precision_digits=2) != 0:
+
+    @api.onchange("price_unit", "sale_order_line_history_ids")
+    def _onchange_price(self):
+        self.warning = '/'
+        if self.sale_order_line_history_ids:
+            if tools.float_compare(self.sale_order_line_history_ids[0].price_unit, self.price_unit, precision_digits=2) != 0:
                 self.warning = 'Unit Price difference than last unit price'
